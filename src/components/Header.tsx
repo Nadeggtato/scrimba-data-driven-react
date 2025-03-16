370e611e6e9ce163b9b4c47194c8cac50ea4608c
ia <paepulumbarit@gmail.com>
2025-03-16 12:35:17 -0700

    Add header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faEarthAmericas } from "@fortawesome/free-solid-svg-icons"
+import { Inter } from 'next/font/google'
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Header() {
   return (
     <header>
       <FontAwesomeIcon icon={faEarthAmericas}/>
-      <h1>My Travel Journal</h1>
+      <h1 className={inter.className}>my travel journal.</h1>
     </header>
   )
 }
